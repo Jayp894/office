ec949d974ac683365f970b60c98badd60386e98e
Jayp894 <63721083+Jayp894@users.noreply.github.com>
2021-05-14 11:15:03 +0530

    Add files via upload

---
+++ template.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
+
+  render(){
+    return(
+      <div class="App">
+        <header>
+
+        </header>
+        <body>
+          
+        </body>
+      </div>
+    );
+  }
+
+}
+
+
+export default App;
